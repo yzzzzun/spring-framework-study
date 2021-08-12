adce8c0c61e4c5c7ea4dec8c256b3d89ef7177e0
youngjun.jin <yzzzzun@gmail.com>
2021-08-12 11:09:44 +0900

    feat: study interceptor

--- src/main/java/com/yzzzzun/spring/WebConfig.java
+++ src/main/java/com/yzzzzun/spring/WebConfig.java
@@ -5,15 +5,18 @@ import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.yzzzzun.spring.filter.FirstLogFilter;
 import com.yzzzzun.spring.filter.SecondLogFilter;
+import com.yzzzzun.spring.interceptor.FirstLogInterceptor;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Bean
-	public FilterRegistrationBean firstLogFilter(){
+	public FilterRegistrationBean firstLogFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new FirstLogFilter());
 		filterRegistrationBean.setOrder(1);
@@ -22,11 +25,19 @@ public class WebConfig {
 	}
 
 	@Bean
-	public FilterRegistrationBean secondLogFilter(){
+	public FilterRegistrationBean secondLogFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new SecondLogFilter());
 		filterRegistrationBean.setOrder(2);
 		filterRegistrationBean.addUrlPatterns("/*");
 		return filterRegistrationBean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new FirstLogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "/*.ico", "/error");
 	}
 }
